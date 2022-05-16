863f9ea2db2d5afc5b5d7dca9a591cf789e6adcb
JeonSeongHee <wjstjd1004@naver.com>
2022-05-16 21:03:26 +0900

    [3-2주차] 외부입력 진행중(컬러까지 받기)

--- GLSLBase/Renderer.cpp
+++ GLSLBase/Renderer.cpp
@@ -72,12 +72,26 @@ void Renderer::CreateVertexBufferObjects()
 	float lecture2[]
 		=
 	{
-		0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0,
+		0.0, 0.0, 0.0,
+		1.0, 0.0, 0.0,
+		1.0, 1.0, 0.0,
 	}; // 9 points array
 
 	glGenBuffers(1, &m_VBOLecture2);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture2), lecture2, GL_STATIC_DRAW);
+
+	float lecture3[]
+		=
+	{
+		0.0, 0.0, 0.0, 1, 0, 0, 1,
+		1.0, 0.0, 0.0, 0, 1, 0, 1,
+		1.0, 1.0, 0.0, 0, 0, 1, 1,
+	}; // 21 points array
+
+	glGenBuffers(1, &m_VBOLecture3);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture3), lecture3, GL_STATIC_DRAW);
 }
 
 void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -338,13 +352,24 @@ void Renderer::Lecture3()
 {
 	GLuint shader = m_Lecture3Shader;
 	glUseProgram(shader);
+	// position
 	int attribPosition = glGetAttribLocation(shader, "a_Position");
 	glEnableVertexAttribArray(attribPosition);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
-	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
+	// color
+	int attribColor = glGetAttribLocation(shader, "a_Color");
+	glEnableVertexAttribArray(attribColor);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribColor, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (GLvoid*)(sizeof(float) * 3));
 
+	// 외부입력 포지션
 	int uniformLocTime = glGetUniformLocation(shader, "u_Time");
 	glUniform1f(uniformLocTime, gTime);
+
+	// 외부입력 컬러
+	int unifromLocColor = glGetUniformLocation(shader, "u_Color");
+	glUniform4f(unifromLocColor, 1, 1, 1, 1);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
