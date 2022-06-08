ce56f5879e1f5ec395ecd38c8619ce3b70d12bdd
JeonSeongHee <wjstjd1004@naver.com>
2022-06-09 01:26:09 +0900

    [6-3주차] 6-2주차에 적용한 텍스처의 좌표를 다양하게 꼬아보기 (무조건 시험에 나옴)

--- GLSLBase/Shaders/TextureSandbox.fs
+++ GLSLBase/Shaders/TextureSandbox.fs
@@ -15,7 +15,66 @@ vec4 Flip()
 	return returnValue;
 }
 
+vec4 P1()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.y = abs(2.0 * (v_TexCoord.y - 0.5));
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+
+vec4 P2()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(v_TexCoord.x * 3.0);
+	newTexCoord.y = newTexCoord.y / 3.0 + floor(v_TexCoord.x * 3.0) / 3.0;
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+
+vec4 P3()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(v_TexCoord.x * 3.0);
+	newTexCoord.y = floor((1 - v_TexCoord.x) * 3.0) / 3.0 + v_TexCoord.y / 3.0f;
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+
+vec4 p4()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.y = floor((1 - v_TexCoord.y) * 3.0) / 3.0 + fract(v_TexCoord.y * 3.0) / 3.0; 
+
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+
+vec4 p5()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(v_TexCoord.x * 2.0) + floor(v_TexCoord.y * 2.0) * 0.5;
+	newTexCoord.y = fract(v_TexCoord.y * 2.0);
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+
+vec4 p6()
+{
+	vec4 returnValue = vec4(0);
+	vec2 newTexCoord = v_TexCoord;
+	newTexCoord.x = fract(v_TexCoord.x * 2.0);
+	newTexCoord.y = fract(v_TexCoord.y * 2.0) + floor(v_TexCoord.x * 2.0) * 0.5;
+	returnValue = texture(u_TexSampler, newTexCoord);
+	return returnValue;
+}
+ 
 void main()
 {
-	FragColor = Flip();
+	FragColor = p6();
 }
